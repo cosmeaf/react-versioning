2ffcd60cf1fa143a53db883f2739c1ad198c4957
“Cosme <Cosme.alex@gmail.com>
2023-10-22 10:22:59 -0300

    Build: Sua descrição da construção aqui

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import packageJson from "../package.json";
 function App() {
   return (
     <div className="App">
-      <span className="font-bold text-black">{packageJson.version}</span>{" "}
+      <span className="font-bold text-black">{packageJson.version}</span>
+      <p>git commit -m "Fet: Sua descrição da correção aqui"</p>
+      <p>git commit -m "Fix: Sua descrição da correção aqui"</p>
+      <p>git commit -m "Build: Sua descrição da correção aqui"</p>
     </div>
   );
 }
